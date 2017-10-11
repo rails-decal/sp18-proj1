2330f85c32daeacc7adbadf0d8f2557e213745a5
Jonathan Chu <jonmchu@berkeley.edu>
2017-10-11 16:48:01 -0700

    edited seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
 #
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# Make Pokemon
+%w(Squirtle Charmander Bulbasaur Pikachu).each do |name|
+  Pokemon.create name: name, level: rand(1..20)
+end
+
+# Make other trainers
+%w(Ash Gary Misty Brock).each do |name|
+  Trainer.create name: name, email: name+"@pokeportal.com", password: 'password'
+end
