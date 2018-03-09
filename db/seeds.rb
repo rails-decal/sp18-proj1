8add46b9321b076988f3c538dd699186809b6d98
Ken Chen <lbkchen@gmail.com>
2018-03-08 21:31:05 -0800

    Adds all pokemon files and new seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,13 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 # Make Pokemon
-%w(Squirtle Charmander Bulbasaur Pikachu).each do |name|
-  Pokemon.create name: name, level: rand(1..20)
+[['Squirtle', 7], ['Charmander', 4], ['Bulbasaur', 1], ['Pikachu', 25]].each do |name, ndex|
+  Pokemon.create(
+    name: name,
+    level: rand(1..20),
+    health: 100,
+    ndex: ndex,
+  )
 end
 
 # Make other trainers
